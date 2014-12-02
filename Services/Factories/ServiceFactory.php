06e90c80cc4b113cdbc1426b8a72cf06be0dd6e3
Fabien <Fabien@splio-fabien-imac-2.local>
2014-12-02 17:02:14 +0100

    Adds a service factory

---
+++ Services/Factories/ServiceFactory.php
@@ -0,0 +1,71 @@
+<?php
+
+// Namespace
+
+namespace Zero\Services\Factories;
+
+
+/**
+ *
+ */
+
+class ServiceFactory
+{
+	/**
+	 *
+	 */
+	
+	public function buildService($serviceCode, $definitions)
+	{
+		//
+
+		if (array_key_exists($serviceCode, $definitions) === false)
+		{
+			e(EXCEPTION_SERVICE_NOT_FOUND);
+		}
+
+
+		//
+
+		$definition = $definitions[$serviceCode];
+
+		
+		//
+
+		$path = PATH_ROOT . $definition['path'] . '.php';
+		$className = $definition['classname'];
+
+
+		//
+
+		if (file_exists($path) === false)
+		{
+			e(EXCEPTION_SERVICE_NOT_FOUND);
+		}
+		
+		
+		//
+		
+		require_once($path);
+
+
+		//
+		
+		$reflection = new \ReflectionClass($className);
+
+		if ($reflection->isInstantiable() === false)
+		{
+			e(EXCEPTION_SERVICE_NOT_FOUND);
+		}
+
+
+		// Create the service
+		
+		$service = new $className();
+
+
+		return $service;
+	}
+}
+
+?>
