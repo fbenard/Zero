c7d1dc3c6d67aed57708bffadb998e118e6439db
Fabien <Fabien@splio-fabien-imac.local>
2014-12-10 11:15:38 +0100

    Adds CLI scripts

---
+++ Core/zero-cli.php
@@ -0,0 +1,46 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ *
+ */
+
+function install()
+{
+	/*
+	rm -f /usr/local/bin/zapp
+	ln -s /var/www/app/Components/fbenard/zero/Core/zero-app.php /usr/local/bin/zapp ; sudo chmod 755 /usr/local/bin/zapp
+	*/
+}
+
+
+/**
+ *
+ */
+
+function create()
+{
+	/*
+	- Application
+		- Preferences
+			- Boot.json
+	- index.php
+	- .htaccess
+	*/
+}
+
+
+/**
+ *
+ */
+
+function main()
+{
+}
+
+
+//
+
+main();
+
+?>
