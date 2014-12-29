4600be52351a6ea541ae3a8380dd9f2d5cbeb269
Fabien <Fabien@splio-fabien-imac.local>
2014-12-29 14:08:02 +0100

    Improves index.php

--- Core/Templates/Application/index.php
+++ Core/Templates/Application/index.php
@@ -6,7 +6,15 @@ $pathToZero = dirname(getcwd()) . '/Components/fbenard/zero/Core/zero-boot.php';
 
 if (file_exists($pathToZero) === false)
 {
-	die('*** ERROR: Cannot find Zero.');
+	if (php_sapi_name() === 'cli')
+	{
+		print("\033[1;31m*** Cannot find Zero\033[0;0m\n");
+		exit(1);
+	}
+	else
+	{
+		die("*** ERROR: Cannot find Zero.\n");
+	}
 }
 else
 {
