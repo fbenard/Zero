12dd2dc174531f808c654992e9ec00d2bb787515
Fabien <Fabien@splio-fabien-imac.local>
2015-01-12 18:19:10 +0100

    Fixes view renderer

--- Services/Renderers/ViewRenderer.php
+++ Services/Renderers/ViewRenderer.php
@@ -11,29 +11,20 @@ namespace Zero\Services\Renderers;
 
 class ViewRenderer
 {
-	// Attributes
-
-	private $_view = null;
-
-
-	/**
-	 *
-	 */
-
-	public function __construct()
-	{
-		$this->_view = new \Zero\Classes\View();
-	}
-
-
 	/**
 	 *
 	 */
 
 	public function renderView($viewCode, $viewArguments = null)
 	{
-		// Render the view
 		
+		// Build a view
+		
+		$this->_view = new \Zero\Classes\View();
+
+
+		// Render the view
+
 		$result = $this->_view->render($viewCode, $viewArguments);
 
 
