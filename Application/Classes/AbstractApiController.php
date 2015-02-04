cd19bdc3ecb43d4e4b66fee8d916324e83cb261f
Fabien <Fabien@splio-fabien-imac.local>
2015-02-04 13:20:37 +0100

    Introduces an API controller

---
+++ Application/Classes/AbstractApiController.php
@@ -0,0 +1,48 @@
+<?php
+
+// Namespace
+
+namespace fbenard\Zero\Classes;
+
+
+/**
+ *
+ */
+
+abstract class AbstractApiController
+extends \fbenard\Zero\Classes\AbstractController
+{
+	/**
+	 *
+	 */
+
+	public function __construct()
+	{
+		// Parent constructor
+
+		parent::__construct();
+
+
+		// Build attributes
+
+		$this->_response->setHeaders
+		(
+			[
+				'Content-Type' => 'application/json; charset=UTF-8',
+				'Cache-Control' => 'private, no-cache, no-store, must-revalidate'
+			]
+		);
+	}
+
+
+	/**
+	 *
+	 */
+
+	protected function setOutput($output)
+	{
+		$this->_output = json_encode($output);
+	}
+}
+
+?>
