803b03c118f5970b78630ff576066589c8e35331
Fabien Bénard <fb.spam@me.com>
2015-04-25 09:26:44 +0200

    Uses mustache to render viezs

--- Application/Services/Renderers/ViewRenderer.php
+++ Application/Services/Renderers/ViewRenderer.php
@@ -11,21 +11,55 @@ namespace fbenard\Zero\Services\Renderers;
 
 class ViewRenderer
 {
+	// Attributes
+
+	private $_mustache = null;
+
+
+	/**
+	 *
+	 */
+
+	public function __construct()
+	{
+		$this->_mustache = new Mustache_Engine();
+	}
+
+
 	/**
 	 *
 	 */
 
 	public function renderView($viewCode, $viewArguments = null)
 	{
+		// Build the path to the view
+
+		$pathToView = PATH_APPLICATION . 'Views/' . $viewCode;
+
 		
-		// Build a view
-		
-		$this->_view = new \fbenard\Zero\Classes\View();
+		// Make sure the view exists
+
+		if (file_exists($pathToView) === false)
+		{
+			\z\e
+			(
+				EXCEPTION_VIEW_NOT_FOUND,
+				[
+					'viewCode' => $viewCode,
+					'viewArguments' => $viewArguments
+				]
+			);
+		}
+
+
+		// Load the view
+
+		$view = file_get_contents($pathToView);
 
 
 		// Render the view
 
-		$result = $this->_view->render($viewCode, $viewArguments);
+		$result = $this->_mustache->render($view, $viewArguments);
 
 
 		return $result;
