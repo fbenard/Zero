d778b3adf23d7821136d193644c0c47aba8a1665
Fabien Bénard <fbenard.public@gmail.com>
2015-07-31 10:05:07 +0200

    Improves JSON factory

--- Application/Services/Factories/JsonFactory.php
+++ Application/Services/Factories/JsonFactory.php
@@ -17,12 +17,31 @@ class JsonFactory
 	
 	public function encodeJson($json)
 	{
-		//
-		
+		// Encode
+
 		$result = json_encode
 		(
 			$json,
-			JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+			JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+		);
+
+
+		return $result;
+	}
+
+
+	/**
+	 *
+	 */
+	
+	public function decodeJson($json)
+	{
+		// Decode
+
+		$result = json_decode
+		(
+			$json,
+			true
 		);
 
 
