d7690c948330c3b4fa15f1cb7077b6a74a78fd6d
Fabien Bénard <fbenard.public@gmail.com>
2016-08-22 11:00:30 +0200

    AbstractException takes care of context

--- App/Exceptions/ControllerNotFoundException.php
+++ App/Exceptions/ControllerNotFoundException.php
@@ -12,19 +12,6 @@ namespace fbenard\Zero\Exceptions;
 class ControllerNotFoundException
 extends \fbenard\Zero\Classes\AbstractException
 {
-	// Attributes
-
-	private $_controllerCode = $controllerCode;
-
-	
-	/**
-	 *
-	 */
-
-	public function __construct($controllerCode)
-	{
-		$this->_controllerCode = $controllerCode;
-	}
 }
 
 ?>
