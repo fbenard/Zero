4c5f162a9b4d4ffff1a6b04a2fa8e755323f21ab
Fabien Bénard <fbenard.public@gmail.com>
2016-09-11 19:55:25 +0200

    Adds JsonEncodeException

---
+++ App/Exceptions/JsonEncodeException.php
@@ -0,0 +1,27 @@
+<?php
+
+// Namespace
+
+namespace fbenard\Zero\Exceptions;
+
+
+/**
+ *
+ */
+
+class JsonEncodeException
+extends \fbenard\Zero\Classes\AbstractException
+{
+	/**
+	 *
+	 */
+
+	public function __construct($json, $error)
+	{
+		// Call parent constructor
+
+		parent::__construct($json, $error);
+	}
+}
+
+?>
