522f08bbf4c5a641ae1515beb83364181ab46637
Fabien Bénard <fabien@benard.co>
2017-05-03 08:32:27 +0200

    Removes factory code

--- App/Services/Managers/BootManager.php
+++ App/Services/Managers/BootManager.php
@@ -28,69 +28,8 @@ implements \fbenard\Zero\Interfaces\Managers\BootManager
 	/**
 	 *
 	 */
-
-	private function fixBoot($boot)
 	{
-		// Ensure boot is an array
 
-		if (is_array($boot) === false)
-		{
-			$boot = [];
-		}
-
-
-		// Ensure boot has the expected structure
-
-		$boot = array_merge
-		(
-			[
-				'cache' => [],
-				'dependencies' => [],
-				'hosts' => [],
-				'environment' => null
-			],
-			$boot
-		);
-
-
-		return $boot;
-	}
-
-
-	/**
-	 *
-	 */
-
-	private function fixDependencies($dependencies)
-	{
-		// Ensure dependencies is an array
-
-		if (is_array($dependencies) === false)
-		{
-			$dependencies = [];
-		}
-
-
-		//
-
-		foreach ($dependencies as &$dependency)
-		{
-			$dependency = PATH_COMPONENTS . $dependency;
-		}
-
-
-		// Inject Zero and the application
-
-		array_unshift($dependencies, PATH_ZERO);
-		array_push($dependencies, PATH_APP);
-		
-
-		// De-duplicate dependencies
-
-		$dependencies = array_unique($dependencies);
-
-
-		return $dependencies;
 	}
 
 
@@ -153,35 +92,6 @@ implements \fbenard\Zero\Interfaces\Managers\BootManager
 				$this->_environment = $host['environment'];
 			}
 		}
-	}
-
-
-	/**
-	 *
-	 */
-
-	private function loadBoot()
-	{
-		// Load Boot.json
-
-		$boot = null;
-		$pathToBoot = PATH_APP . '/Config/Boot.json';
-
-		if (file_exists($pathToBoot) === true)
-		{
-			// Decode Boot.json
-
-			$rawBoot = file_get_contents($pathToBoot);
-			$boot = json_decode($rawBoot, true);
-		}
-
-
-		// Fix boot
-
-		$boot = $this->fixBoot($boot);
-
-
-		return $boot;
 	}
 }
 
